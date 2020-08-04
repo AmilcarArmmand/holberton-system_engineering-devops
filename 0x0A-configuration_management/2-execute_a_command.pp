6c8eb7bac0385740251630fb1d059be51410a6b4
Amilcar Armmand <armmand@gmail.com>
2020-08-04 00:18:36 +0000

    added provider and command details

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
 #Using Puppet, create a manifest that kills a process named killmenow
 
-exec { 'pkill -p killmenow':
-  path    => '/usr/bin',
+exec { 'pkill killmenow':
+  command  => 'pkill killmenow',
+  path     => '/usr/bin:/usr/sbin:/bin',
+  provider => shell,
 }
