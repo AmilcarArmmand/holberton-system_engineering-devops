372f5700cd4b534541a54473a4699bef32d6d9ca
AmilcarArmmand <armmand@gmail.com>
2021-04-07 03:56:49 +0000

    added task 7 untested

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,40 @@
+# Puppet script requirements
+# Nginx should be listening on port 80
+# GET request using curl, must return a page that contains 'Holberton School'
+# The redirection must be a “301 Moved Permanently”
+
+exec { 'apt-get update':
+  command  => 'sudo apt-get update',
+  path     => '/usr/bin',
+  provider => shell
+}
+
+package { 'nginx':
+  ensure => present,
+}
+
+file { [ '/etc', '/etc/nginx', '/etc/nginx/sites-available', '/var', '/var/www', '/var/www/html', '/var/www/err', '/var/www/err/html' ]:
+  ensure => 'directory',
+}
+
+file { 'index':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => "Holberton School\n",
+}
+
+file { '404':
+  ensure  => present,
+  path    => '/var/www/html/404.html',
+  content => "Ceci n'est pas une page\n",
+}
+
+file { 'nginx-config':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  content => "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\n\tserver_name _;\nrewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\t}\n\\terror_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}"
+}
+
+exec { 'restart nginx':
+  command => '/usr/sbin/service restart nginx',
+}
