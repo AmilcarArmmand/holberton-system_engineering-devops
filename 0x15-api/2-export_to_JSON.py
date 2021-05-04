9be9f420816f340ef55dbf4f880796fdbf4f1fe5
AmilcarArmmand <armmand@gmail.com>
2021-05-04 01:11:05 +0000

    updated to fix order of objects

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,22 +8,24 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    userId = argv[1]
+    u_id = argv[1]
     # request for user data
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId)).json()
+                        format(u_id)).json()
     todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId)).json()
+                        format(u_id)).json()
 
     username = user.get("username")
-    task_list = []
-    item_dict = {}
+
+    new_list = []
+    task_dict = {}
     for task in todo:
-        item_dict["task"] = task.get('title')
-        item_dict["completed"] = task.get('completed')
-        item_dict["username"] = user.get('username')
-        task_list.append(item_dict)
+        task_dict["task"] = task.get("title")
+        task_dict["completed"] = task.get("completed")
+        task_dict["username"] = username
+        new_list.append(task_dict)
+        task_dict = {}
     jso = {}
-    jso[userId] = task_list
-    with open("{}.json".format(userId), 'w') as f:
+    jso[u_id] = new_list
+    with open("{}.json".format(u_id), 'w') as f:
         json.dump(jso, f)
