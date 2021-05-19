8b9f49cfe39f959e05c25a0d82f4218ebb5f5860
Amilcar <armmand@gmail.com>
2021-05-18 20:37:32 -0400

    fixed pep8 errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,8 +8,8 @@ from requests import get
 def top_ten(subreddit):
     try:
         resp = get("https://www.reddit.com/r/{}/hot.json".format(subreddit),
-                  headers={'User-agent': 'Mozilla/5.0'}, params={'limit': 10},
-                  allow_redirects=False).json()
+                   headers={'User-agent': 'Mozilla/5.0'}, params={'limit': 10},
+                   allow_redirects=False).json()
         for item in resp['data']['children']:
             print(item['data']['title'])
     except:
