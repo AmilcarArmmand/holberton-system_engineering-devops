baec1cb5c7286f950ec8a8a0c0f09528e8c7ffa6
Amilcar <armmand@gmail.com>
2021-05-18 20:06:16 -0400

    refactored task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,8 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     """gets the number of subscribers"""
-    try:
-        subs = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-               headers={'User-agent': 'Mozilla/5.0'}, allow_redirects=False).json()
-        return subs.get('data').get('subscribers') if not None else 0
-    except:
-        return 0
+    resp = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+            headers={'User-agent': 'Mozilla/5.0'}, allow_redirects=False)
+    data = resp.json().get('data', {'subscribers': 0})
+    subs = data.get('subscribers', 0)
+    return subs
