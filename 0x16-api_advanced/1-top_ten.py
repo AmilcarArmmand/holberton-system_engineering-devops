d77faae1936122447ec2babc5cd7979220c2770d
Amilcar <armmand@gmail.com>
2021-05-18 20:21:23 -0400

    added task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+from requests import get
+
+
+def top_ten(subreddit):
+    try:
+        resp = get("https://www.reddit.com/r/{}/hot.json".format(subreddit),
+                  headers={'User-agent': 'Mozilla/5.0'}, params={'limit': 10},
+                  allow_redirects=False).json()
+        for item in resp['data']['children']:
+            print(item['data']['title'])
+    except:
+        print('None')
