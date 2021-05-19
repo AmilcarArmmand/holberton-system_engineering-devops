7a48c9d2ad2749c5aff5c208fd5f67f9fda070f2
Amilcar <armmand@gmail.com>
2021-05-18 20:35:58 -0400

    fixed pep8 errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ from requests import get
 def number_of_subscribers(subreddit):
     """gets the number of subscribers"""
     resp = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-            headers={'User-agent': 'Mozilla/5.0'}, allow_redirects=False)
+               headers={'User-agent': 'Mozilla/5.0'}, allow_redirects=False)
     data = resp.json().get('data', {'subscribers': 0})
     subs = data.get('subscribers', 0)
     return subs
